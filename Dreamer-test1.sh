9c46281e52234881ad5a25bbb275adfb67f83746
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-09-06 19:21:30 +0200

    e2openplugins: remove from script https://github.com/OpenPLi/openpli-oe-core/commit/612c99033732a95aece2f6ec6c9d2f3f2db853e3

--- Dreamer-test1.sh
+++ Dreamer-test1.sh
@@ -15,6 +15,4 @@ rm -f meta-openpli/recipes-filesystems/smbnetfs/smbnetfs_git.bbappend
 cp -R meta-dream/recipes-daemons/vsftpd/ meta-openpli/recipes-daemons/
 cp -f meta-dream/recipes-connectivity/samba/samba_4.6.%.bbappend meta-openpli/recipes-connectivity/samba/samba_4.%.bbappend
 cp -f meta-dream/recipes-connectivity/inadyn-mt/inadyn-mt.bb meta-openpli/recipes-connectivity/inadyn-mt/inadyn-mt.bb
-cp -R meta-dream/recipes-openpli/e2openplugins-extra/ meta-openpli/recipes-openpli/
-rm -f meta-dream/recipes-openpli/e2openplugins-extra/enigma2-plugin-extensions-bitrate.bb
 rm -f meta-openpli/recipes-connectivity/autossh/autossh_1.4c.bb
