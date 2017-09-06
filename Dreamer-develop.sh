6519de0c85bbbf3252a00551727a8a028d0bc265
Persian Professionals <persianpros@yahoo.com>
2017-09-06 12:48:18 +0430

    Update Dreamer-develop.sh

--- Dreamer-develop.sh
+++ Dreamer-develop.sh
@@ -9,6 +9,6 @@ echo ""
 echo "You must run this script only once!"
 echo ""
 rm -rf meta-dream
-git clone https://github.com/DMM-PLi/meta-dream.git
+git clone -b develop https://github.com/DMM-PLi/meta-dream.git
 rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
 rm -f meta-openpli/recipes-connectivity/autossh/autossh_1.4c.bb
