f9800f3ff8294475a90ed7e02f90c4ebe3225e59
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-09-06 19:27:48 +0200

    samba: remove from script https://github.com/OpenPLi/openpli-oe-core/commit/9ce036f3c9a6e0f83c1bcbf32152fd095c2ecfbb

--- Dreamer-test1.sh
+++ Dreamer-test1.sh
@@ -13,6 +13,5 @@ git clone -b test1 https://github.com/DMM-PLi/meta-dream.git
 rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
 rm -f meta-openpli/recipes-filesystems/smbnetfs/smbnetfs_git.bbappend
 cp -R meta-dream/recipes-daemons/vsftpd/ meta-openpli/recipes-daemons/
-cp -f meta-dream/recipes-connectivity/samba/samba_4.6.%.bbappend meta-openpli/recipes-connectivity/samba/samba_4.%.bbappend
 cp -f meta-dream/recipes-connectivity/inadyn-mt/inadyn-mt.bb meta-openpli/recipes-connectivity/inadyn-mt/inadyn-mt.bb
 rm -f meta-openpli/recipes-connectivity/autossh/autossh_1.4c.bb
