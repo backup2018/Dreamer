7744d078709b2219070e0daae89ae3e170a8ed30
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-11-17 08:04:22 +0100

    update rocko.sh

--- Dreamer-rocko.sh
+++ Dreamer-rocko.sh
@@ -4,14 +4,12 @@ echo "meta-dream by Hains, Persian Prince and MastaG"
 echo ""
 echo "https://github.com/DMM-PLi"
 echo ""
-echo "rocko branch, Compatible rev: c928613f51af14b6f93332593ee9e8c2bf3b6582"
+echo "rocko branch, Compatible rev: 0391e8ba6d79be14ba9514be75d043fdbafe3cb7"
 echo ""
 echo "You must run this script only once!"
 echo ""
+rm -rf meta-dream
 rm -rf meta-dreambox
 git clone -b rocko https://github.com/DMM-PLi/meta-dream.git
-rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
-rm -f meta-openpli/recipes-connectivity/autossh/autossh_1.4c.bb
 rm -f meta-openpli/recipes-filesystems/smbnetfs/smbnetfs_git.bbappend
-cp -f meta-dream/recipes-connectivity/inadyn-mt/inadyn-mt.bb meta-openpli/recipes-connectivity/inadyn-mt/inadyn-mt.bb
 find -name "Makefile" -type f -exec sed -i 's/meta-dreambox/meta-dream/g' {} \;
