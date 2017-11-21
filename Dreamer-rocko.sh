d54df4791d8ee978caad65e80d3d1a3109bec471
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-11-21 13:15:28 +0100

    update rocko.sh

--- Dreamer-rocko.sh
+++ Dreamer-rocko.sh
@@ -8,8 +8,7 @@ echo "rocko branch, Compatible rev: 0391e8ba6d79be14ba9514be75d043fdbafe3cb7"
 echo ""
 echo "You must run this script only once!"
 echo ""
-rm -rf meta-dream
-rm -rf meta-dreambox
-git clone -b rocko https://github.com/DMM-PLi/meta-dream.git
+cd meta-dream
+git pull origin rocko
+cd ..
 rm -f meta-openpli/recipes-filesystems/smbnetfs/smbnetfs_git.bbappend
-find -name "Makefile" -type f -exec sed -i 's/meta-dreambox/meta-dream/g' {} \;
