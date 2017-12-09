d3faa0d2dd6ceaa0fc747b0e65b3741bec355a13
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-12-09 19:12:55 +0100

    update develop.sh for pyro branch

--- Dreamer-develop.sh
+++ Dreamer-develop.sh
@@ -4,11 +4,13 @@ echo "meta-dream by Hains, Persian Prince and MastaG"
 echo ""
 echo "https://github.com/DMM-PLi"
 echo ""
-echo "develop branch, Compatible rev: d20cc93e707fe09bd8fe9290a5486df3497f6474"
+echo "develop branch, Compatible rev: 4914b474f634cbcb5580d42ae67b25de0b3861be"
 echo ""
 echo "You must run this script only once!"
 echo ""
 rm -rf meta-dream
-git clone -b develop https://github.com/DMM-PLi/meta-dream.git
+git clone -b pyro https://github.com/DMM-PLi/meta-dream.git
 rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
 rm -f meta-openpli/recipes-connectivity/autossh/autossh_1.4c.bb
+rm -f meta-openpli/recipes-filesystems/smbnetfs/smbnetfs_git.bbappend
+cp -f meta-dream/recipes-connectivity/inadyn-mt/inadyn-mt.bb meta-openpli/recipes-connectivity/inadyn-mt/inadyn-mt.bb
