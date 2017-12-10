2f3e28f4beaf41d670232e848a235df76a1e7c6c
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-12-10 11:53:10 +0100

    update develop.sh No need to delete autossh now autossh 1.4c is back online. Update to 1.4e: https://github.com/OpenPLi/openpli-oe-core/pull/365

--- Dreamer-develop.sh
+++ Dreamer-develop.sh
@@ -11,6 +11,5 @@ echo ""
 rm -rf meta-dream
 git clone -b develop https://github.com/DMM-PLi/meta-dream.git
 rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
-rm -f meta-openpli/recipes-connectivity/autossh/autossh_1.4c.bb
 rm -f meta-openpli/recipes-filesystems/smbnetfs/smbnetfs_git.bbappend
 cp -f meta-dream/recipes-connectivity/inadyn-mt/inadyn-mt.bb meta-openpli/recipes-connectivity/inadyn-mt/inadyn-mt.bb
