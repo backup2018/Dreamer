7be158dd841f785fbc780a0e3319ca819776d2a4
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-12-11 07:35:12 +0100

    update develop.sh

--- Dreamer-develop.sh
+++ Dreamer-develop.sh
@@ -4,12 +4,10 @@ echo "meta-dream by Hains, Persian Prince and MastaG"
 echo ""
 echo "https://github.com/DMM-PLi"
 echo ""
-echo "develop branch, Compatible rev: 4914b474f634cbcb5580d42ae67b25de0b3861be"
+echo "develop branch, Compatible rev: a68d952914ee90b13c14f34fdf986dcc9c07dc68"
 echo ""
 echo "You must run this script only once!"
 echo ""
 rm -rf meta-dream
 git clone -b develop https://github.com/DMM-PLi/meta-dream.git
 rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
-rm -f meta-openpli/recipes-filesystems/smbnetfs/smbnetfs_git.bbappend
-cp -f meta-dream/recipes-connectivity/inadyn-mt/inadyn-mt.bb meta-openpli/recipes-connectivity/inadyn-mt/inadyn-mt.bb
