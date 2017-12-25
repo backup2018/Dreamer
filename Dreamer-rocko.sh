f39e469c4f9465b817918bf7b256d3c46aa1471c
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-12-25 21:36:08 +0100

    Update Dreamer-rocko.sh

--- Dreamer-rocko.sh
+++ Dreamer-rocko.sh
@@ -4,10 +4,10 @@ echo "meta-dream by Hains, Persian Prince and MastaG"
 echo ""
 echo "https://github.com/DMM-PLi"
 echo ""
-echo "rocko branch, Compatible rev: 88cd9ae9631ed2f00de69cb7c1caeed64d54595c"
+echo "rocko branch, Compatible rev: c9265811de5f381db449ec5eb7548bbc8cb943ee"
 echo ""
 echo "You must run this script only once!"
 echo ""
-cd meta-dream
-git pull origin rocko
-cd ..
+rm -rf meta-dream
+git clone -b rocko https://github.com/DMM-PLi/meta-dream.git
+rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
