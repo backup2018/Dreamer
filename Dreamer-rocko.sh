97f8e4361691a556b084e2e27a0fb403a2c77eb4
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2017-12-29 10:49:54 +0100

    Update Dreamer-rocko.sh Using eudev causes enigma2 crash(with dm8000 at least). Switch back to busybox-mdev. This reverts  commit: https://github.com/OpenPLi/openpli-oe-core/commit/1729034b8012fa7b5176e5d66389cbda6a143504

--- Dreamer-rocko.sh
+++ Dreamer-rocko.sh
@@ -4,10 +4,11 @@ echo "meta-dream by Hains, Persian Prince and MastaG"
 echo ""
 echo "https://github.com/DMM-PLi"
 echo ""
-echo "rocko branch, Compatible rev: c9265811de5f381db449ec5eb7548bbc8cb943ee"
+echo "rocko branch, Compatible rev: ce5aa42cd15cba353fa93439f6c2ea42f9121e4f"
 echo ""
 echo "You must run this script only once!"
 echo ""
 rm -rf meta-dream
 git clone -b rocko https://github.com/DMM-PLi/meta-dream.git
 rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
+find meta-openpli/conf/distro/ -name "openpli.conf" -type f -exec sed -i 's/eudev/busybox-mdev/g' {} \;
