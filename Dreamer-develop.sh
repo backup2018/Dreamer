8555ff6836adeb440b222c3bd47ce6174be3afdf
Persian Professionals <persianpros@yahoo.com>
2018-02-05 15:33:51 +0330

    Update Dreamer-develop.sh

--- Dreamer-develop.sh
+++ Dreamer-develop.sh
@@ -2,12 +2,12 @@
 echo ""
 echo "meta-dream by Hains, Persian Prince and MastaG"
 echo ""
-echo "https://github.com/DMM-PLi"
+echo "https://github.com/PLi-metas"
 echo ""
-echo "develop branch, Compatible rev: b75ccdf7994a29e17ff197b564a201911d1057dd"
+echo "develop branch, Compatible rev: 22cf556e6b2be613ceb86abf3701b9fdb64eceb3"
 echo ""
 echo "You must run this script only once!"
 echo ""
 rm -rf meta-dream
-git clone -b develop https://github.com/DMM-PLi/meta-dream.git
+git clone -b develop https://github.com/PLi-metas/meta-dream.git
 rm -f meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugin-extensions-openwebif.bbappend
